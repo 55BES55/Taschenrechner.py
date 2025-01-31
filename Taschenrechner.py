3754be4eae0ae841c3a650856331ab74d02d0367
Bekir <bekiremresen04@gmail.com>
2025-02-01 00:53:06 +0100

    Name der Zahlen variablen verändert für eine Bessere Lesbarkeit
    Eingabeaufforderungen hinzugefügt für bessere Benutzbarkeit
    
    While schleife hinzugefüht damit man das programm nicht nach einer berechnung komplett neu starten muss.
    
    Division by Zero error gefixt um nicht nicht so einen fehler zuzulassen
    
    Funktion erstellt für eine bessere übersicht
    
    f strings benutzt für übersichtlicheren code
    
    Unzulässige operatoren verhindert für solideren code
    
    while schleife fürs sichere beenden oder neustarten des programms
    
    division fehler behoben war vorher blockiert

--- Taschenrechner.py
+++ Taschenrechner.py
@@ -6,23 +6,42 @@ print('''Willkommen beim Taschenrechner!
      Dividieren "/"
     Gib dafür einfach deine erste zahl ein danach deinen gewünschten operanten und dann die zweite zahl
     ''')
-while True:
-    num1 = float(input("Eingabe erste Zahl:"))
-    operant = input("Eingabe Operator:")
-    num2 = float(input("Eingabe zweite Zahl:"))
-
-    if operant == "+":
-        print(num1 + num2)
-    elif operant == "-":
-        print(num1 - num2)
-    elif operant == "*":
-        print(num1 * num2)
-    elif operant == "/":
+def berechnung(num1, operator, num2):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
         if num2 == 0:
-            print("Fehler: Division durch 0!")
+            return "Fehler: Division durch 0!"
         else:
-            print(num1 / num2)
+            return num1 / num2
+    else:
+        return f'Unzulässiger Operator {operator} Es sind nur +,-,*,/'
+
+#Anfang des Codes
+while True:
+    try:
+        num1 = float(input("Eingabe erste Zahl:"))
+        operator = input("Eingabe Operator:")
+        num2 = float(input("Eingabe zweite Zahl:"))
+
+        solution = berechnung(num1, operator, num2)
+        print(f"Ergebnis: {solution}")
+    except ValueError:
+        print("Fehler bei der eingabe")
 
 
-    if input("Nochmal? (j/n) ")  != "j":
-        break
+    while True:
+        answer = input("Nochmal? (j/n): ").lower()
+        if answer not in ("j", "n"):
+            print("Ungültige angabe! nur (j/n).")
+        else:
+            break
+    if answer != "j":
+       break
+
+
+
