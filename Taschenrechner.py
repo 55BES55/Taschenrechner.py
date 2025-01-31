1aadadc13ea09913d0dcdbd3a8360c4bb2550374
Bekir <bekiremresen04@gmail.com>
2025-01-31 23:44:43 +0100

    Name der Zahlen variablen verändert für eine Bessere Lesbarkeit
    Eingabeaufforderungen hinzugefügt für bessere Benutzbarkeit
    
    While schleife hinzugefüht damit man das programm nicht nach einer berechnung komplett neu starten muss.
    
    Division by Zero error gefixt um nicht nicht so einen fehler zuzulassen

--- Taschenrechner.py
+++ Taschenrechner.py
@@ -6,16 +6,23 @@ print('''Willkommen beim Taschenrechner!
      Dividieren "/"
     Gib dafür einfach deine erste zahl ein danach deinen gewünschten operanten und dann die zweite zahl
     ''')
+while True:
+    num1 = float(input("Eingabe erste Zahl:"))
+    operant = input("Eingabe Operator:")
+    num2 = float(input("Eingabe zweite Zahl:"))
 
-num1 = float(input("Eingabe erste Zahl:"))
-operant = input("Eingabe Operator:")
-num2 = float(input("Eingabe zweite Zahl:"))
+    if operant == "+":
+        print(num1 + num2)
+    elif operant == "-":
+        print(num1 - num2)
+    elif operant == "*":
+        print(num1 * num2)
+    elif operant == "/":
+        if num2 == 0:
+            print("Fehler: Division durch 0!")
+        else:
+            print(num1 / num2)
 
-if operant == "+":
-    print(num1 + num2)
-elif operant == "-":
-    print(num1 - num2)
-elif operant == "*":
-    print(num1 * num2)
-elif operant == "/":
-    print(num1 / num2)
+
+    if input("Nochmal? (j/n) ")  != "j":
+        break
